1c982e5f897ad5e7e6eab72253f8a7437a4c9abd
Fernando Bezerra <fernandosouzasaga@gmail.com>
2026-08-29 22:13:43 +0000

    Remove setup.sh (obsoleto) e pasta extra (nao utilizada)

--- monitoring/setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-kubectl apply -f prometheus-config.yaml
-kubectl apply -f jolokia-exporter.yaml
-kubectl apply -f node-exporter.yaml
-kubectl apply -f prometheus-deployment.yaml
-echo "Tudo aplicado com sucesso!"
